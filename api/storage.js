505cf28cfbabc27c4eb6c3876de67db4ea96eee8
Seigh sword <zack.yt.7085@gmail.com>
2026-02-12 10:25:40 +0000

    commiting again

--- api/storage.js
+++ api/storage.js
@@ -1,6 +1,20 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+ 
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    // Handle the "preflight" request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
 
     const TOKEN = process.env.BOT_TOKEN;
     const CHAT_ID = process.env.CHAT_ID;
@@ -19,5 +33,6 @@ module.exports = async (req, res) => {
             return res.status(500).json({ error: 'Telegram API Error' });
         }
     }
+    
     res.status(405).json({ error: 'Only POST allowed' });
 };
